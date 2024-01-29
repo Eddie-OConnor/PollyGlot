1b3588bd5155f690f4dedc670e79d86422de0802
Eddie-OConnor <eddie.oconnor3@gmail.com>
2024-01-29 17:51:21 -0500

    Update fetchApi.js

--- netlify/functions/fetchApi/fetchApi.js
+++ netlify/functions/fetchApi/fetchApi.js
@@ -15,7 +15,7 @@ const handler = async (event) => {
     } catch (error) {
         return { statusCode: 500, body: error.toString() }
     }
-    }
+}
       
     
-    module.exports = { handler }
+module.exports = { handler }
